1c814c4604e7d5fe40ee04e480fd73d65bad59b0
Rabsha Shakeel <rabshasiddiqui@gmail.com>
2024-02-20 14:50:13 +0500

    first commit

---
+++ src/component/Randomquote.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Randomquote = () => {
+    return(
+        <div className="mainbox">
+            <h1>Rabsha Shakeel</h1>
+        </div>
+    )
+}
+
+export default Randomquote;
