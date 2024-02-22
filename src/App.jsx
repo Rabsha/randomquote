8fd20a3b7ceadc303c4487f92ddd49b3dc9934c9
Rabsha Shakeel <rabshasiddiqui@gmail.com>
2024-02-22 09:44:55 +0500

    “final”

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Randomquote from './component/Randomquote'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
   return (
     <>
-      
+      <Randomquote/>
     </>
   )
 }
--- src/component/Randomquote.jsx
+++ src/component/Randomquote.jsx
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import axios from "axios";
 
 const Randomquote = () => {
+
+    const Quotations = <FontAwesomeIcon icon={faQuoteLeft} />
+
+    const [quotes, setQuotes] = useState('');
+    const colorData = ['#613a43','#726a95','#f39c12','#a96971','#36384c','#849974','#bdbb99'];
+    const [randomColor, setRandomColor] = useState('');
+
+    const HandleQuotes = () => {
+        axios.get('https://api.quotable.io/random')
+        .then(response => {
+            setQuotes(response.data);
+        })
+        .catch(error =>{
+            console.error(error);
+        });
+    }
+
+    useEffect(() => {
+        HandleQuotes();
+    },[]);
+
+    const PressForQuotes = () => {
+        HandleQuotes();
+        const randomIndex = Math.floor(Math.random() * colorData.length);
+        setRandomColor(colorData[randomIndex]);
+    }
+
     return(
+        <body style={{backgroundColor:randomColor, transition:"ease-in-out 1s"}}>
         <div className="mainbox">
-            <h1>Rabsha Shakeel</h1>
+            <div className="container">
+                <div className="mainboxsquare">
+                    <div className="myboxquote">
+                        <div className="datahead">
+                            <h1 style={{color:randomColor,transition:"ease-in-out 1s"}}>Random Quotes</h1>
+                        </div>
+                        <div className="quotes">
+                            <h4 style={{color:randomColor,transition:"ease-in-out 1s"}}>{Quotations} {quotes.content}</h4>
+                            <h5 style={{color:randomColor,transition:"ease-in-out 1s"}}>"{quotes.author}"</h5>
+                        </div>
+                        <div className="renderquote">
+                            <button className="mybtnwork bgwork" style={{backgroundColor:randomColor,transition:"ease-in-out 1s"}} onClick={PressForQuotes}>New Quote</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        </body>
     )
 }
 
-export default Randomquote;
+export default Randomquote
